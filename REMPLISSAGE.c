045abf630e80960bdd51f0802cb6b92873598c13
martinlbx <martin.loubinoux@gmail.com>
2025-10-28 15:15:08 +0100

    Add remplissage function to populate grid
    Implement filling logic for the grid with random characters.

--- REMPLISSAGE.c
+++ REMPLISSAGE.c
@@ -1,1 +1,31 @@
+#include "paramitz.h"
 
+void remplissage(char grille[ligne][colonne]){
+    int i;
+    char vide;
+    srand(time(NULL));
+    for(i=0;i<colonne;i++){
+        if(grille[0][i]==' '){
+            vide=1+rand()%5;
+            if(vide==1){
+                grille[i][j]='S';
+            }
+            else if(vide==2){
+                grille[i][j]='F';
+            }
+            else if(vide==3){
+                grille[i][j]='P';
+            }
+            else if(vide==4){
+                grille[i][j]='O';
+            }
+            else if(vide==5){
+                grille[i][j]='M';
+            }
+
+        }
+
+    }
+
+
+}
