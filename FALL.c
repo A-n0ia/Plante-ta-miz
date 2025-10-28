792b5dedd7a6baf3a5c9ee2f02c3ee54db37b26f
martinlbx <martin.loubinoux@gmail.com>
2025-10-28 18:57:29 +0100

    Add files via upload

---
+++ FALL.c
@@ -0,0 +1,19 @@
+void fall(char grille[ligne][colonne]){
+    int i,j,k,cpt;
+    char
+    for(i=ligne;i>0;i--){
+        for(j=0;j<colonne;j++){
+            if(grille[i][j]==' '){
+                    cpt=i;
+                    k=0;
+                    while(cpt!=0){
+                        grille[i+k][j]=grille[i+k+1][j];
+                        k++;
+                        cpt--;
+                    }
+
+            }
+        }
+    }
+
+}
