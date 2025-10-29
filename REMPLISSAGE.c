ec731b82c4178eaabb1be1fbc64821bfec78a66c
martinlbx <martin.loubinoux@gmail.com>
2025-10-29 17:13:55 +0100

    Update REMPLISSAGE.c

--- REMPLISSAGE.c
+++ REMPLISSAGE.c
@@ -4,30 +4,28 @@ void remplissage(char grille[ligne][colonne]){
     int i,j;
     char vide;
     srand(time(NULL));
-    for(i=0;i<1;i++){
         for(j=0;j<colonne;j++)
             if(grille[i][j]==' '){
-            vide=1+rand()%5;
-            if(vide==1){
-                grille[i][j]='S';
+                vide=1+rand()%5;
+                if(vide==1){
+                    grille[i][j]='S';
+                }
+                else if(vide==2){
+                    grille[i][j]='F';
+                }
+                else if(vide==3){
+                    grille[i][j]='P';
+                }
+                else if(vide==4){
+                    grille[i][j]='O';
+                }
+                else if(vide==5){
+                    grille[i][j]='M';
+                }
             }
-            else if(vide==2){
-                grille[i][j]='F';
-            }
-            else if(vide==3){
-                grille[i][j]='P';
-            }
-            else if(vide==4){
-                grille[i][j]='O';
-            }
-            else if(vide==5){
-                grille[i][j]='M';
-            }
-
-        }
-        
-    }
-
 }
+
+
+
 
 
