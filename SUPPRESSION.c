9809845da3b75cca6cdbfc51586e04b9474b5fc0
martinlbx <martin.loubinoux@gmail.com>
2025-10-29 15:55:10 +0100

    Update SUPPRESSION.c

--- SUPPRESSION.c
+++ SUPPRESSION.c
@@ -2,7 +2,6 @@
 
 void suppression(char grille[ligne][colonne],int* score){  //Regarde si une des règles de suppression et vérifiée, auquel cas remplace tous les items concernés par des ' ' dans plateau
     int i,j,k,a,b;
-    char swap;
     for(i=0;i<ligne;i++){
         for(j=0;j<colonne;j++){
             //FAIRE DES FONCTIONS POUR CHAQUE MANIERES DE GAGNER DES POINTS ? 
@@ -13,22 +12,16 @@ void suppression(char grille[ligne][colonne],int* score){  //Regarde si une des 
                 while(grille[i][j]==grille[i][j+k]){
                     k++;
                     if(k>=5){
-                        swap=grille[i][j];
-                        for(a=0;a<ligne;a++){
-                            for(b=0;b<colonne;b++){
-                                if(grille[a][b]==swap){
-                                    grille[a][b]=' ';
-                                    *score++;
-                                }
-                            }
-                        }
+                        suppr6();
                     }
-                    else if(k>=3){
+                 }
+            }  
+            else if(k>=3){
                 *score=*score+4;
-                }
-            
             }
-            }
+        }           
+    }
+}
 
                 // VERIF EN LIGNE
 
@@ -58,18 +51,19 @@ void suppression(char grille[ligne][colonne],int* score){  //Regarde si une des 
         }
         //IL MANQUE LE H + LE RECTANGLE 
         
-        //FAIRE TOMBER LES ITEMS :
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        //FAIRE TOMBER LES ITEMS : FONCTION FALL
+ 
     }
-
+void suppr6(char grille[ligne][colonne],int* score){
+    int i,j;
+    char swap;
+    swap=grille[i][j];
+    for(i=0;i<ligne;i++){
+        for(j=0;j<colonne;j++){
+            if(grille[i][j]==swap){
+                grille[i][j]=' ';
+                *score++;
+            }
+        }
+    }
+}
