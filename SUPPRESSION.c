bd092e85fc7af6c30452aec2ae576556679ce2ec
martinlbx <martin.loubinoux@gmail.com>
2025-10-29 16:39:21 +0100

    Update SUPPRESSION.c

--- SUPPRESSION.c
+++ SUPPRESSION.c
@@ -4,63 +4,56 @@ void suppression(char grille[ligne][colonne],int* score){  //Regarde si une des 
     int i,j,k,a,b;
     for(i=0;i<ligne;i++){
         for(j=0;j<colonne;j++){
-            verifLigne();
-            verifColonne();
+            verifLigne(grille,i,j,score);
+            verifColonne(grille,i,j,score);
         }           
     }
- 
 }
 
-void suppr6(char grille[ligne][colonne],int* score){
-    int i,j;
-    char swap;
-    swap=grille[i][j];
-    for(i=0;i<ligne;i++){
-        for(j=0;j<colonne;j++){
-            if(grille[i][j]==swap){
-                grille[i][j]=' ';
-                (*score)++;
-            }
-        }
-    }
-}
-
-void verifLigne(char grille[ligne][colonne],int* score){
+void verifLigne(char grille[ligne][colonne],int* score){    //VERIFIER SI IL Y A UN COMBO DE 4 OU 6 SUR UNE LIGNE
     if(grille[i][j]==grille[i][j+1]){
         k=0;
         while(grille[i][j]==grille[i][j+k]){
            k++;
-               if(k>=5){
-                   suppr6();
-                }
+           if(k>=5){
+               char swap;
+               swap=grille[i][j];
+               suppr6(grille,swap,score);
+            }
+            else if(k>=3){
+               *score=*score+4;
+            }
         }
-    }  
-    else if(k>=3){
-        *score=*score+4;
-    }
+    }   
 }
 
-void verifColonne(char grille[ligne][colonne],int* score){
+void verifColonne(char grille[ligne][colonne],int* score){        //VERIFIER SI IL Y A UN COMBO DE 4 OU 6 SUR UNE COLONNE
     if(grille[i][j]==grille[i+1][j]){
         k=0;
         while(grille[i][j]==grille[i+k][j]){
             k++;
             if(k>=5){
                 swap=grille[i][j];
-                    for(a=0;a<ligne;a++){
-                        for(b=0;b<colonne;b++){
-                            if(grille[a][b]==swap){
-                                grille[a][b]=' ';                                    
-                                (*score)++;
-                            }
-                        }
-                    }
+                char swap;
+                swap=grille[i][j];
+                suppr6(grille,swap,score);
             }
             else if(k>=3){
-                        *score=*score+4;
-                        
+                *score=*score+4;
             } 
-                   
+        }         
+    }
+}
+
+
+void suppr6(char grille[ligne][colonne],int* score,char swap){        //SUPPRIMER TOUT LES ITEMS EN CAS DE COMBO 6 
+    int i,j;
+    for(i=0;i<ligne;i++){
+        for(j=0;j<colonne;j++){
+            if(grille[i][j]==swap){
+                grille[i][j]=' ';
+                (*score)++;
+            }
         }
     }
 }
