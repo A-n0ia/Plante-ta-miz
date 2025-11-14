35a0a9399dfe8c2733ee8066bac7b67621640f34
A-n0ia <106909238+A-n0ia@users.noreply.github.com>
2025-11-14 11:31:01 +0100

    New functions adding more details and game mechanics

--- ITEMS.c
+++ ITEMS.c
@@ -1,22 +1,10 @@
 #include "paramitz.h"
-
-
-void ITEMS(char grille[ligne][colonne],int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){
-	int rand_item;
-	rand_item=rand()%101;
-	if(rand_item<=99){
-		canne_sucre(grille,xJoueur,Yjoueur,scoreS,scoreF,scoreP,scoreO,scoreM);   // SUPPR DU 5X5 
-	}
-	else if(rand_item<=100){
-		nuke_grille(grille,scoreS,scoreF,scoreP,scoreO,scoreM);   // SUPPR TOUTE LA GRILLE
-	}
-}
-
-void canne_sucre(char grille[ligne][colonne],int* xJoueur,int* yJoueur,int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){  // SUPPR DU 5X5 
+/*
+void canne_sucre(char grille[ligne][colonne],int* xJoueur,int* yJoueur,int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){  // SUPPR DU 5X5
 	int i,j;
 	for(i=xJoueur-2;i<=xJoueur+2;i++){
 		for(j=yJoueur-2;j<=yJoueur+2;j++){
-		
+
 			if(grille[i][j]=='S') (*scoreS)++;
 			else if(grille[i][j]=='F') (*scoreF)++;
 			else if(grille[i][j])=='P' (*scoreP)++;
@@ -24,19 +12,33 @@ void canne_sucre(char grille[ligne][colonne],int* xJoueur,int* yJoueur,int* scor
 			else if(grille[i][j])=='M' (*scoreM)++;
 			grille[i][j]=' ';
 		}
-	}	
+	}
 }
 
-void nuke_grille(char grille[ligne][colonne],int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){  
+void nuke_grille(char grille[ligne][colonne],int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){
 	int i,j; 									//DEFINE LIGNE/COLONNE POUR NV 2 ET 3
-	for(i=0;i<ligne;i++){     
+	for(i=0;i<ligne;i++){
 		for(j=0;j<colonne;j++){
 			if(grille[i][j]=='S') (*scoreS)++;
 			else if(grille[i][j]=='F') (*scoreF)++;
 			else if(grille[i][j])=='P' (*scoreP)++;
 			else if(grille[i][j])=='O' (*scoreO)++;
 			else if(grille[i][j])=='M' (*scoreM)++;
-			grille[i][j]=' ';			
+			grille[i][j]=' ';
 		}
 	}
 }
+
+
+void ITEMS(char grille[ligne][colonne],int* scoreS, int* scoreF, int* scoreP, int* scoreO, int* scoreM){
+	int rand_item;
+	rand_item=rand()%101;
+	if(rand_item<=99){
+		canne_sucre(grille,xJoueur,Yjoueur,scoreS,scoreF,scoreP,scoreO,scoreM);   // SUPPR DU 5X5
+	}
+	else if(rand_item<=100){
+		nuke_grille(grille,scoreS,scoreF,scoreP,scoreO,scoreM);   // SUPPR TOUTE LA GRILLE
+	}
+}
+*/
+
--- animation_level.c
+++ animation_level.c
@@ -1,8 +1,23 @@
 #include "paramitz.h"
 
-void animation_nv(int level){
-	int i,j;
-	if(level==2){
+void animation_nv(char nom[20],int level){
+	int j,i=0;
+	if(level == 1){
+        gotoxy(20,7);
+        printf("Bienvenue \e[4;31m");
+        while(nom[i]!='\0'){
+			printf("%c",nom[i]);
+			Sleep(200);
+			i++;
+		}
+        printf(reset" !  Vous entrez maintenant dans le 1er niveau !");
+        gotoxy(42,9);
+        printf(jaune "Bonne chance"reset);
+        gotoxy(6,13);
+	}
+
+
+	else if(level==2){
 		char mot[30]="NEXT LEVEL UNLOCKED : 2";
 		for(j=0;j<15;j++){
 			printf("\n");
@@ -10,14 +25,14 @@ void animation_nv(int level){
 		for(j=0;j<6;j++){
 			printf("\t");
 		}
-	
+
 		for(i=0;i<25;i++){
 			printf("%c",mot[i]);
 			Sleep(150);
 		}
-		sleep(10000);
-		}
-	if(level==3){
+		gotoxy(5,25);
+    }
+	else if(level==3){
 		char mot[30]="NEXT LEVEL UNLOCKED : 3";
 		for(j=0;j<15;j++){
 			printf("\n");
@@ -25,17 +40,14 @@ void animation_nv(int level){
 		for(j=0;j<6;j++){
 			printf("\t");
 		}
-	
+
 		for(i=0;i<25;i++){
 			printf("%c",mot[i]);
 			Sleep(120);
 		}
-		Sleep(10000);
-		}
-	
+    }
+
 }
-
-
 
 
 
